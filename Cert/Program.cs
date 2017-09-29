cc4bd36c07afbc92e0ad8143226b6751b47c7cce
Dima <d.a.shimanov@gmail.com>
2017-09-29 15:45:07 +0700

    Full project

---
+++ Cert/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Cert
+{
+    class Program
+    {
+        static int Main()
+        {
+            SqlConnection sqlConnection = new SqlConnection("Server=localhost; Initial Catalog=ESPP; Integrated Security = SSPI;");
+            
+            SqlCommand command = new SqlCommand
+            {
+                CommandText = "select * from certificates where keyid = 'HDBU0H87E0CG0IIE'",
+                CommandType = CommandType.Text,
+                Connection = sqlConnection
+            };
+
+            try
+            {
+                sqlConnection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                object result = null;
+                while (reader.Read())
+                {
+                    result = reader.GetValue(0);
+                }
+
+                sqlConnection.Close();
+
+                if (result == null)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            catch (Exception)
+            {
+                return 100;   
+            }
+        }
+    }
+}
